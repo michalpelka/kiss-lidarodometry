7de1c6554275062ddddf9f2565213a27ec843463
Michał Pełka <michalpelka@gmail.com>
2025-06-29 23:13:11 +0200

    Mp/hotfix v0.3 (#10)
    * hotfix crash on imu estimation outside SE3 group
    
    Signed-off-by: Michal Pelka <michalpelka@gmail.com>
    
    * use fitToSE3
    
    Signed-off-by: Michal Pelka <michalpelka@gmail.com>
    
    ---------
    
    Signed-off-by: Michal Pelka <michalpelka@gmail.com>

--- main.cpp
+++ main.cpp
@@ -420,16 +420,29 @@ void IcpButton()
 
 
                 const double query_time1 = frame.timestamp_hardware.back();
-                const double query_time2 = frame.timestamp_hardware.front();
-                const Eigen::Affine3d imuPose1 {getInterpolatedPose(globals::imuTrajectory, query_time1)};
-                const Eigen::Affine3d imuPose2 {getInterpolatedPose(globals::imuTrajectory, query_time2)};
-                const Eigen::Affine3d imuPose = imuPose1.inverse() * imuPose2;
-                auto imuUpdate = Sophus::SE3d(imuPose.rotation(), imuPose.translation());
-                imuUpdate = imuUpdate.inverse();
-                if (globals::params.useImu)
+                const double query_time2 = query_time1 + globals::params.timestamp_per_icp;
+                const auto m1 = getInterpolatedPose(globals::imuTrajectory, query_time1);
+                const auto m2 = getInterpolatedPose(globals::imuTrajectory, query_time2);
+                if (!m1.isZero() && !m2.isZero())
                 {
-                    icp.delta() = Sophus::SE3 (imuPose.rotation(), imuPose.translation());
+                    const Eigen::Affine3d imuPose1 {m1};
+                    const Eigen::Affine3d imuPose2 {m2};
+
+                    const Eigen::Affine3d imuPose = imuPose1.inverse() * imuPose2;
+                    auto imuUpdate = Sophus::SE3d::fitToSE3(imuPose.matrix());
+                    rotationIMU << std::setprecision(20) << frame.timestamp_hardware.front()  << "," << imuUpdate.inverse().log().transpose().format(CSVFormat)<< std::endl;
+                    if (globals::params.useImu)
+                    {
+                        auto tangentImu = imuUpdate.log();
+                        auto tangentDelta = icp.delta().log();
+                        tangentDelta[3] = tangentImu[3]; // keep z rotation
+                        tangentDelta[4] = tangentImu[4]; // keep x rotation
+                        tangentDelta[5] = tangentImu[5]; // keep y rotation
+                        icp.delta() =  Sophus::SE3d::exp(tangentDelta);
+                    }
+
                 }
+
                 if (globals::params.useGNSSSpeed)
                 {
                     // get speed at query_time2
@@ -450,7 +463,6 @@ void IcpButton()
                     }
 
                 }
-                rotationIMU << std::setprecision(20) << frame.timestamp_hardware.front()  << "," << imuUpdate.log().transpose().format(CSVFormat)<< std::endl;
 
                 auto [registered_frame, registered_frame_timestamps] = icp.RegisterFrame(frame.points, frame.timestamps_offset);
 
